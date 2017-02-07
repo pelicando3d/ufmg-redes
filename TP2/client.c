ae57ed791c88a22fef89d3825f7af098153e563c
Junio Cezar <juniocezar.dcc@gmail.com>
2017-02-07 06:52:17 -0300

    -

--- TP2/client.c
+++ TP2/client.c
@@ -15,128 +15,127 @@ void error(char *msg) {
     exit(0);
 }
 
+
+int send_datagram(int so, char* buff, int buff_len, so_addr* to_addr){
+    int ret;
+    ret = tp_sendto(so, buff,  buff_len,  (struct sockaddr *)to_addr);
+    return ret;
+}
+
+int receive_datagram(int so, char* buff, int buff_len, so_addr* from_addr){
+    int ret;
+    ret = tp_recvfrom(so, buff, buff_len,  (struct sockaddr *)from_addr);
+    return ret;
+}
+
 int main(int argc, char* argv[]) {
-  int sock;
-  int status;
-  struct addrinfo sainfo, *psinfo;
-  struct sockaddr_in6 sin6;
-  int sin6len;
-  char buffer[MAXBUF];
+    int sock;
+    int status;
+    struct addrinfo sainfo, *psinfo;
+    struct sockaddr_in6 sin6;
+    int sin6len;
+    char buffer[MAXBUF];
+  
+    int sockfd, portno, n;
+    int serverlen;
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
+    char *hostname;
+    char *buf;
+    int buffer_size;
+    int window_size;
+    int filename_size;
+    char *filename, msg_toSend;  
+    int bytesLeft;
+    int sendPosition = 0;
+    int chunck_size;
+    int numBytesSent;
+    char *copyName;
 
-  int sockfd, portno, n;
-  int serverlen;
-  struct sockaddr_in serveraddr;
-  struct hostent *server;
-  char *hostname;
-  char *buf;
-  int buffer_size;
-  int window_size;
-  int filename_size;
-  char *filename, msg_toSend;  
-  int bytesLeft;
-  int sendPosition = 0;
-  int chunck_size;
-  int numBytesSent;
-  char *copyName;
-
-   /* check command line arguments */
-   if (argc != 6) {
-      fprintf(stderr,"Uso: %s <nome ou IPv6 do servidor> <porto> <nome_arquivo ><tam_buffer> <tam_janela>\n", argv[0]);
-      exit(0);
-   }
+    /* check command line arguments */
+    if (argc != 6) {
+        fprintf(stderr,"Uso: %s <nome ou IPv6 do servidor> <porto> <nome_arquivo ><tam_buffer> <tam_janela>\n", argv[0]);
+        exit(0);
+    }
 
 
-  hostname = argv[1];
-  portno = atoi(argv[2]);
-  filename = argv[3];
-  buffer_size = atoi(argv[4]);
-  window_size = atoi(argv[5]);
-  filename_size = strlen(filename);    
-  buf = (char*) malloc(buffer_size * sizeof(char));
-  copyName = (char*) malloc(filename_size + 5 * sizeof(char));
-  strcpy(copyName, "copy_");
-  strcat(copyName, filename);
+    hostname = argv[1];
+    portno = atoi(argv[2]);
+    filename = argv[3];
+    buffer_size = atoi(argv[4]);
+    window_size = atoi(argv[5]);
+    filename_size = strlen(filename);    
+    buf = (char*) malloc(buffer_size * sizeof(char));
+    copyName = (char*) malloc(filename_size + 5 * sizeof(char));
+    strcpy(copyName, "copy_");
+    strcat(copyName, filename);
   
 
-  tp_init();
-
-  sock = tp_socket(portno);
-
-  if(sock < 0){     
-    printf("Error %d\n", sock); // Tem que corrigir o erro -1 do bind
-  }
-
-  memset(&sainfo, 0, sizeof(struct addrinfo));
-   
-  sainfo.ai_flags = 0;
-  sainfo.ai_family = PF_INET6;
-  sainfo.ai_socktype = SOCK_DGRAM;
-  sainfo.ai_protocol = IPPROTO_UDP;
-  status = getaddrinfo(hostname, argv[2], &sainfo, &psinfo);
-   
-  bzero(buf, buffer_size);
-  printf("Requesting file: %s (%d)\n", filename, filename_size);
-  msg_toSend = malloc((filename_size+5) * sizeof(char)); // Alocando variavel msg_toSend que tera o filename + id de fim de string
-  strcat(filename, "\\0");
-  bytesLeft = strlen(filename);
-    
-  while(bytesLeft > 0){      
-      chunck_size = bytesLeft > buffer_size ? buffer_size : bytesLeft;
-      memcpy(buf, filename + sendPosition, chunck_size);      
-      numBytesSent = tp_sendto(sock, buf,  chunck_size, (struct sockaddr *)psinfo->ai_addr);
+    tp_init();
+  
+    sock = tp_socket(portno);
+  
+    if(sock < 0){     
+      printf("Error %d\n", sock); // Tem que corrigir o erro -1 do bind
+    }
+  
+    memset(&sainfo, 0, sizeof(struct addrinfo));
+     
+    sainfo.ai_flags = 0;
+    sainfo.ai_family = PF_INET6;
+    sainfo.ai_socktype = SOCK_DGRAM;
+    sainfo.ai_protocol = IPPROTO_UDP;
+    status = getaddrinfo(hostname, argv[2], &sainfo, &psinfo);
+     
+    bzero(buf, buffer_size);
+    printf("Requesting file: %s (%d)\n", filename, filename_size);
+    msg_toSend = malloc((filename_size+5) * sizeof(char)); // Alocando variavel msg_toSend que tera o filename + id de fim de string
+    strcat(filename, "\\0");
+    bytesLeft = strlen(filename);
       
-      printf("Sent:            %s \t ", buf);
-      if (numBytesSent < 0) 
-        error("ERROR in sendto");      
-      sendPosition += numBytesSent;
-      bytesLeft -= numBytesSent;        
-      printf("Bytes Sent: %d  -  Bytes Left: %d\n",numBytesSent , bytesLeft);
-      memset(buf, '\0', strlen(buf)*sizeof(buf));
- }    
-  printf("File requested\n");
+    while(bytesLeft > 0){      
+        chunck_size = bytesLeft > buffer_size ? buffer_size : bytesLeft;
+        memcpy(buf, filename + sendPosition, chunck_size);      
+        numBytesSent = send_datagram(sock, buf,  chunck_size, (struct sockaddr *)psinfo->ai_addr);
+        
+        printf("Sent:            %s \t ", buf);
+        if (numBytesSent < 0) 
+          error("ERROR in sendto");      
+        sendPosition += numBytesSent;
+        bytesLeft -= numBytesSent;        
+        printf("Bytes Sent: %d  -  Bytes Left: %d\n",numBytesSent , bytesLeft);
+        memset(buf, '\0', strlen(buf)*sizeof(buf));
+    }    
 
+    printf("File requested\n");
 
-
-
-  int totalBytesRcvd = 0;
-
-    memset(buf, '\0', sizeof(buf));
-
-    
-
-    FILE *file = fopen(copyName, "w+"); // file to be downloaded will be written here
-  
+    /* Abrindo arquivo para gravacao */
+    FILE *file = fopen(copyName, "w+");  
     if (file == NULL) // Invalid file or file not found
-      printf("erro abrir");
+        printf("erro abrir");
+
+
     int times = 0;
     char ack[30];
+    int totalBytesRcvd = 0;
+    memset(buf, '\0', sizeof(buf));
     while (1) {
-      //n = recvfrom(sockfd, buf, buffer_size, 0, &serveraddr, &serverlen);
-      n = tp_recvfrom(sock, buf, buffer_size,  (struct sockaddr *)psinfo->ai_addr);
-      times++;
-//      printf("%d (%d)", n, times);
-      if (n == 0)
-        break;
-      if (n < 0)
-        error("ERROR in recvfrom");
-      if (n > 1 && buf[n-2] == '\\' && buf[n-1] == '0')
-        break;
-
-      
-      /*if (times >= 2056){
-        printf("\n\n%s", buf);
-      }*/
-
-      fwrite(buf, 1, n, file); // writes file
-      totalBytesRcvd += n;
-      sprintf(ack, "ACK - %d", times);
-      //numBytesSent = sendto(sockfd, ack, 30, 0, &serveraddr, serverlen);
-      numBytesSent = tp_sendto(sock, ack,  30,  (struct sockaddr *)psinfo->ai_addr);
+        n = receive_datagram(sock, buf, buffer_size,  (struct sockaddr *)psinfo->ai_addr);
+        times++;
+        if (n == 0)
+          break;
+        if (n < 0)
+          error("ERROR in recvfrom");
+        if (n > 1 && buf[n-2] == '\\' && buf[n-1] == '0')
+          break;
+  
+        fwrite(buf, 1, n, file); // writes file
+        totalBytesRcvd += n;
+        sprintf(ack, "ACK - %d", times);
+        numBytesSent = send_datagram(sock, ack,  30,  (struct sockaddr *)psinfo->ai_addr);
 
     }
     fclose(file);
-
-
 
 
   // free memory
--- TP2/server.c
+++ TP2/server.c
@@ -7,9 +7,7 @@
 #include <stdlib.h>
 #include "tp_socket.h"
 
-#define MAXBUF 65536
 
-#define BUFSIZE 1024
 typedef enum { false, true } bool;
 
 void error(char *msg) {
@@ -18,200 +16,190 @@ void error(char *msg) {
 }
 
 
+int send_datagram(int so, char* buff, int buff_len, so_addr* to_addr){
+    int ret;
+    ret = tp_sendto(so, buff,  buff_len,  (struct sockaddr *)to_addr);
+    return ret;
+}
 
-int main(int argc, char **argv)
-{
-   int sock;
-   int status;
-   struct sockaddr_in6 sin6;
-   int sin6len;
-   char buffer[MAXBUF];
-
-   int sockfd; // socket
-  int portno; // id da porta
-  int clientlen; /* byte size of client's address */
-  int buffer_size; // tamanho do buffer
-  int window_size; // tamanho da janela
-  char *dir; // diretorio a ser utilizado
-  char *filepath;
-  struct sockaddr_in serveraddr; // endereco do servidor
-  struct sockaddr_in clientaddr; // endereco do cliente
-  struct hostent *hostp; // informacoes do cliente
-  char *buf; // buffer p/ mensagem
-  char *hostaddrp; /* dotted decimal host addr string */
-  int optval; /* flag value for setsockopt */
-  int n; /* message byte size */
-  so_addr  addr;
-  char *hostname;
-
-  // Checar argumentos da linha de comando
-  if (argc != 5) {
-    fprintf(stderr, "Uso corrento: %s <porto do servidor> <tam_buffer> <tam_janela> <diretório a ser utilizado>\n", argv[0]);
-    exit(1);
-  }
-  portno = atoi(argv[1]);
-  buffer_size = atoi(argv[2]);
-  window_size = atoi(argv[3]);
-  dir = argv[4];
-  filepath = argv[4];
-
-  
-
-  tp_init();
-  sock = tp_socket(portno);
-
-  
-  buf = (char*) malloc(buffer_size * sizeof(char));
-
-   /* setsockopt: Handy debugging trick that lets 
-   * us rerun the server immediately after we kill it; 
-   * otherwise we have to wait about 20 secs. 
-   * Eliminates "ERROR on binding: Address already in use" error. 
-   */
-  optval = 1;
-  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-       (const void *)&optval , sizeof(int));
+int receive_datagram(int so, char* buff, int buff_len, so_addr* from_addr){
+    int ret;
+    ret = tp_recvfrom(so, buff, buff_len,  (struct sockaddr *)from_addr);
+    return ret;
+}
 
 
 
+int main(int argc, char **argv){
+    int sock;
+    int status;
+    struct sockaddr_in6 sin6;
+    int sin6len;   
+    int portno; // id da porta
+    int buffer_size; // tamanho do buffer
+    int window_size; // tamanho da janela
+    char *dir; // diretorio a ser utilizado
+    char *filepath; // path pro arquivo a ser enviado
+    char *buf; // buffer p/ mensagem
+    char *hostaddrp;     
+    int n; // bytesEnviados
 
-  memset(&sin6, 0, sin6len);
-  sin6len = sizeof(struct sockaddr_in6);
-  sin6.sin6_port = htons(portno);
-  sin6.sin6_family = AF_INET6;
-  sin6.sin6_addr = in6addr_any;
-  status = getsockname(sock, (struct sockaddr *)&sin6, &sin6len);
-
-  char msg[9064];
-  strcpy(msg, "");
-  bool signal = false;
-  int totalBytes = 0;
-  int totalBytesRec = 0;
-
-  while (1) {
-    bzero(buf, buffer_size);
-    totalBytesRec = 0;
-    totalBytes = 0;
-
-    memset(msg, '\0', sizeof(msg));
-    memset(buf, '\0', sizeof(buf));    
-    memset(filepath, '\0', sizeof(filepath));    
-
-    printf("%s - %s \n\n", msg, buf);
-    printf("\t\tState 1\n");
-    //while( (n = recvfrom(sockfd, buf, buffer_size, 0, (struct sockaddr *) &clientaddr, &clientlen)) ){    
-    while( (n = tp_recvfrom(sock, buf, buffer_size, (struct sockaddr *) &sin6)) ){    
-
-      totalBytes += n;
-      totalBytesRec += strlen(buf);
-      if (n < 0)
-        error("ERROR in recvfrom");
-      
-      buf[n] = '\0';
-      strcat(msg, buf); //msg += buffer;
-
-      if(signal && buf[n-1] == '0') 
-        break;
-      if (n > 1 && buf[n-2] == '\\' && buf[n-1] == '0')
-        break;
-      if(buf[n-1] == '\\')
-        signal = true;
-
-
+    /* Checar argumentos da linha de comando */
+    if (argc != 5) {
+      fprintf(stderr, "Uso corrento: %s <porto do servidor> <tam_buffer> <tam_janela> <diretório a ser utilizado>\n", argv[0]);
+      exit(1);
     }
 
-    msg[strlen(msg)-2] = '\0';
-    printf("server received %d/%d bytes: %s\tsize of string: %d\n", totalBytesRec, totalBytes, msg, strlen(msg));
+    /* Transferindo argumentos */
+    portno = atoi(argv[1]);
+    buffer_size = atoi(argv[2]);
+    window_size = atoi(argv[3]);
+    dir = argv[4];
+    filepath = argv[4];
   
+    
+    /* Iniciando TP */
+    
+    tp_init();
+    sock = tp_socket(portno);    
+    buf = (char*) malloc(buffer_size * sizeof(char));
+       
+    /* Inicializando estrutura de enderecos e protocolos*/
+
+    memset(&sin6, 0, sin6len);
+    sin6len = sizeof(struct sockaddr_in6);
+    sin6.sin6_port = htons(portno);
+    sin6.sin6_family = AF_INET6;
+    sin6.sin6_addr = in6addr_any;
+    status = getsockname(sock, (struct sockaddr *)&sin6, &sin6len);
+  
+    
+    /* Recebendo o Nome do arquivo a ser enviado */
+    char *msg;
+    bool signal = false;
+    int totalBytes = 0;
+    int totalBytesRec = 0;
+
+    msg = malloc(buffer_size * sizeof(char));    
+    strcpy(msg, "");
+
+    /* Recebe nome do arquivo em chuncks, pois nome do arquivo pode
+    *  ser maior do que o buffer disponivel para envio no cliente
+    */    
+  
+    while (1) {
+        bzero(buf, buffer_size);
+        totalBytesRec = 0;
+        totalBytes = 0;
+    
+        memset(msg, '\0', sizeof(msg));
+        memset(buf, '\0', sizeof(buf));    
+        memset(filepath, '\0', sizeof(filepath));    
+    
+        printf("\t\tState 1\n");
+        while( (n = receive_datagram(sock, buf, buffer_size, (struct sockaddr *) &sin6)) ){    
+            totalBytes += n;
+            totalBytesRec += strlen(buf);
+            if (n < 0)
+              error("ERROR in recvfrom"); // ToDo: Enviar para uma funcao de error
+            
+            buf[n] = '\0';
+            strcat(msg, buf); //msg += buffer;
+      
+            if(signal && buf[n-1] == '0') 
+              break;
+            if (n > 1 && buf[n-2] == '\\' && buf[n-1] == '0')
+              break;
+            if(buf[n-1] == '\\')
+              signal = true;    
+            msg = realloc(msg, (strlen(msg)+n) * sizeof(char*));
+            // ToDo: checar se memoria foi alocada de fato
+      }
+  
+    msg[strlen(msg)-2] = '\0'; // remove o sinal de fim de mensagem \0 incluido no nome do arquivo
+    printf("server received %d/%d bytes: %s\tsize of string: %d\n", totalBytesRec, totalBytes, msg, strlen(msg));
+
 
     /* 
      * Servidor recebeu nome do arquivo, agora vai enviar para o cliente
      */
 
+    /* cruando Path para arquivo baseado no diretorio e filename*/
     strcat(filepath, dir);
     strcat(filepath, msg);
-    FILE *file = fopen(filepath, "rb"); // open in binary mode
 
-    char line[12];
-    char *result;
 
-    
+    FILE *file = fopen(filepath, "rb"); 
+
     if(file == NULL){
-      printf("ERRO ABRI FILE %s", dir);
-      exit(2);
+        printf("ERRO ABRI FILE %s", dir);
+        exit(2); //Todo: passar pra funcao de erro
     }
     else
-      printf("Arquivo %s aberto com sucesso\n", dir);
-
-
-    //unsigned int bytesRead = 0;
+        printf("Arquivo %s aberto com sucesso\n", dir);
+  
+  
+    
     unsigned int totalBytesSent = 0;
     unsigned int totalMsgSent = 0;
-    int  numBytesSent;
-    
+    int  numBytesSent;    
     ssize_t nElem = 0;
+    int chunck_size;    
+    int times = 0;    
+    bool initiated = false;
+    char ack[30]; // "ok: num_seq"
 
-    int chunck_size;
 
-    //cout << "Enviando arquivo: " << filename << " para o cliente.\n ";
-    int times = 0;
     printf("\t\tState 2\n");
-    int initiated = 0;
-    char ack[30];
-    while(1) {
-      printf("Lendo\n");
-      nElem = fread(buf, sizeof(char), buffer_size, file); 
-    
-      chunck_size =  nElem > buffer_size ? buffer_size : nElem;
 
-      printf("chuck size: %d, strlen buf: %d, buf size: %d, nElen: %d\n", chunck_size, strlen(buf), buffer_size, nElem);
 
-      if (nElem <= 0 && !feof(file)) { 
-        printf("Erro Leitura\n");
-        break; // error leitura
+    /* enviando arquivo em chunks */
+    while(1) {        
+        nElem = fread(buf, sizeof(char), buffer_size, file);       
+        chunck_size =  nElem > buffer_size ? buffer_size : nElem;  
+        printf("chuck size: %d, strlen buf: %d, buf size: %d, nElen: %d\n", chunck_size, strlen(buf), buffer_size, nElem);
+  
+        if (nElem <= 0 && !feof(file)) { 
+          printf("Erro Leitura\n");
+          break; // error leitura Todo: mandar pra funcao
+        }
+  
+   /*     if(initiated == true){
+           //n = recvfrom(sockfd, ack, 30, 0, (struct sockaddr *) &clientaddr, &clientlen);
+           n = tp_recvfrom(sockfd, ack, 30, (struct sockaddr *) &clientaddr);
+           printf("%s\n", ack);
+        }*/
+
+        numBytesSent = send_datagram(sock, buf,  chunck_size, (struct sockaddr *) &sin6);
+  
+        initiated = true;
+        times++;
+  
+        if (numBytesSent < 0){
+          printf("Erro Envio\n");
+          break; // error sento
+        }else{
+          totalBytesSent += numBytesSent;
+          totalMsgSent += 1;
+        }
+  
+        if(feof(file)){
+          break;
+        }
       }
-
- /*     if(initiated){
-         //n = recvfrom(sockfd, ack, 30, 0, (struct sockaddr *) &clientaddr, &clientlen);
-         n = tp_recvfrom(sockfd, ack, 30, (struct sockaddr *) &clientaddr);
-         printf("%s\n", ack);
-      }*/
-      //numBytesSent = sendto(sockfd, buf, chunck_size, 0, (struct sockaddr *) &clientaddr, clientlen);
-      numBytesSent = tp_sendto(sock, buf,  chunck_size, (struct sockaddr *) &sin6);
-
-
-      initiated = 1;
+  
+  
       
-      times++;
-
-      if (numBytesSent < 0){
-        printf("Erro Envio\n");
-        break; // error sento
-      }else{
-        totalBytesSent += numBytesSent;
-        totalMsgSent += 1;
-      }
-
-      if(feof(file)){
-        break;
-      }
-    }
-
-
-    char *end = "\\0";
-    printf("\t\tState 3\n");
-    //numBytesSent = sendto(sockfd, end, sizeof(end), 0, (struct sockaddr *) &clientaddr, clientlen);
-    numBytesSent = tp_sendto(sock, end,  sizeof(end), (struct sockaddr *) &sin6);
-    printf("Arquivo: %s enviado para o cliente em %d blocos\n", msg, times);
-
-    fclose(file);    
- 
-
-
-
-   shutdown(sock, 2);
-   close(sock);
-   return 0;
-}
-
-}
+      printf("Arquivo: %s enviado para o cliente em %d blocos\n", msg, times);
+  
+      fclose(file);    
+   
+  
+  
+  
+     shutdown(sock, 2);
+     close(sock);
+     return 0;
+  }
+  
+  }  
