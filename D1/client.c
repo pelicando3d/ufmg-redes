26661acce262db9a4608b992fc4323ec12fdfe7b
junio cezar ribeiro da silva <juniocezar@estocolmo.grad.dcc.ufmg.br>
2017-05-09 14:51:15 -0300

    inserted packing step

--- D1/client.c
+++ D1/client.c
@@ -2,8 +2,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdint.h>
 
-const uint32_t bufsize = 1024;
+
+const uint32_t psize = 1024;
+const uint32_t bufsize = 1024; // min 14 because packet headers
+
 
 void die(uint32_t id) {
   printf("Im dead LoL WoWOOWOW (%u)\n", id);
@@ -11,15 +18,16 @@ void die(uint32_t id) {
 
 //toDo: get server ip by hostname
 void initiate_activeConnection(int *ssocket, struct sockaddr_in* server, char* port, char* server_addr) {
+  printf("Iniciando conexao ativamente [%s|%s].\n", server_addr, port);
   int server_port = atoi(port);
-  *ssocket = socket(AF_INET, SOCK_STREAM, 0);
+  *ssocket = socket(PF_INET, SOCK_STREAM, 0);
   server->sin_family = AF_INET;
-  server->sin_port = htons(server_port); /* Set port number, using htons function to use proper byte order */  
-  server->sin_addr.s_addr = inet_addr(server_addr); /* Set IP address to localhost */  
+  server->sin_port = htons(7891); /* Set port number, using htons function to use proper byte order */  
+  server->sin_addr.s_addr = inet_addr("127.0.0.1"); /* Set IP address to localhost */  
   memset(server->sin_zero, '\0', sizeof server->sin_zero); /* Set all bits of the padding field to 0 */
 
-  socklen_t addr_size = sizeof server;
-  connect(*ssocket, (struct sockaddr *) &server, addr_size);
+  socklen_t addr_size = sizeof *server;
+  connect(*ssocket, (struct sockaddr *) server, addr_size);
 }
 
 
@@ -28,24 +36,57 @@ void send_file(char* filename , int ssocket) {
   char buffer[bufsize]; 
   uint32_t total_bytesSent = 0, total_msgSent = 0;    
   ssize_t nElem = 0;
+  uint32_t SYNC = 0xDCC023C2;
+  uint16_t CHKSUM = 0;
+  uint16_t LENGTH = 0xAAB;
+  uint8_t ID = 1;
+  uint8_t FLAGS = 0; // 8 bits only
+  uint8_t byteArray[bufsize];
+  uint32_t DATA;
+
+  // Packet MIN SIZE is 14 bytes due to headers
 
   input_file = fopen(filename, "rb"); // open in binary mode
-  
   if(input_file == NULL)
     die(8); //file doesnt exist      
 
   printf("Enviando arquivo %s para o cliente %u \n", filename, ssocket);
 
   while(1) {    
-    nElem = fread(&buffer, sizeof(char), bufsize, input_file); //Reading files in chunks of bufsize at most
-    
+    LENGTH = fread(&buffer, sizeof(char), bufsize - 16, input_file); //Reading files in chunks of bufsize at most (-16 frp, header)
     if (nElem <= 0 && !feof(input_file)) { //If I couldnt read anything but I havent reached end of file, there is an error
       die(9); // reading error;
     }
+    printf("READ %d ELEMENTS\n",LENGTH);
+    //buffer[LENGTH] = '\0';
 
-    //toDo: Construct Packet Here, before sending    
-    ssize_t num_bytesSent = send(ssocket, buffer, nElem, 0);
+    // Packet Headers
+    SYNC = htonl(SYNC);
+    CHKSUM = htonl(CHKSUM);
+    LENGTH = htons(LENGTH);
+    //DATA = htonl(buffer);
+
+    printf("PACKET = [%x][%x][%x][%x][%x][%x]\n\n", SYNC, SYNC, CHKSUM, LENGTH, ID, FLAGS);
+
+    memcpy(&byteArray[0], &SYNC, sizeof(SYNC)); // 00 - 31  (4 bytes) : SYNC
+    memcpy(&byteArray[4], &SYNC, sizeof(SYNC)); // 32 - 63  (4 bytes) : SYNC
+    memcpy(&byteArray[8], &CHKSUM, sizeof(CHKSUM)); // 64 - 79  (2 bytes) : CHKSUM
+    memcpy(&byteArray[10], &LENGTH, sizeof(LENGTH));// 80 - 95 (2 bytes) : LENGTH
+    memcpy(&byteArray[12], &ID, sizeof(ID));// 96 - 103 (1 byte) : ID
+    memcpy(&byteArray[13], &FLAGS, sizeof(FLAGS));// 104 - 111 (1 bytes) : FLAGS
+
+    //memcpy(&byteArray[14], &DATA, sizeof(DATA));// 112 - limit (X bytes) : DATA
+
     
+    int i = 0;
+    for(; i < 50; i++) {
+      printf("byte %d (%u) (%x)\n",i, byteArray[i], byteArray[i]);
+    }
+
+    return ;
+    
+    //toDo: Construct Packet Here, before sending    
+    ssize_t num_bytesSent = send(ssocket, buffer, nElem, 0);    
     if (num_bytesSent < 0){ //If i couldnt send anything, there is an error -- Is it RIGHT?
       die(7); //error send
     }else{ //Otherwise, we increment our counter of total sent bytes and total sent packages      
@@ -58,8 +99,9 @@ void send_file(char* filename , int ssocket) {
       printf("Fim do arquivo !\n\n");      
       break;
     }
+    fclose(input_file);
   }
-  printf("File %s sent to %i. Bytes: %u, Packets: %u\n\n", filename, total_bytesSent, total_msgSent);
+  printf("File %s sent to %i. Bytes: %u, Packets: %u\n\n", filename, ssocket, total_bytesSent, total_msgSent);
 }
 
 int main(int argc, char** argv){
@@ -77,8 +119,9 @@ int main(int argc, char** argv){
   
   
   //initiate_passiveConnection(&clientSocket, &serverAddr, c_port, hostaddr);  
-  initiate_activeConnection(&ssocket, &serverAddr, c_port, hostaddr);    
+  //initiate_activeConnection(&ssocket, &serverAddr, c_port, hostaddr);    
   send_file(in_name, ssocket);
+  close(ssocket);
 
 
   /*---- Connect the socket to the server using the address struct ----*/
--- D1/server.c
+++ D1/server.c
@@ -1,43 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdint.h>
+
 
 const uint32_t bufsize = 1024;
 
-void initiate_passiveConnection(int *socket, struct sockaddr_in* server, char* port) {  
-  socket = socket(AF_INET, SOCK_STREAM, 0);
-  /*---- Configure settings of the server address struct ----*/  
-  server->sin_family = AF_INET; /* Address family = Internet */  
-  server->sin_port = htons(port); /* Set port number, using htons function to use proper byte order */  
-  server->sin_addr.s_addr = inet_addr("127.0.0.1"); /* Set IP address to localhost */  
-  memset(server->sin_zero, '\0', sizeof server->sin_zero);  /* Set all bits of the padding field to 0 */
-  bind(socket, (struct sockaddr *) &server, sizeof(server)); /*---- Bind the address struct to the socket ----*/
+void die(uint32_t id) {
+  printf("Im dead LoL WoWOOWOW (%u)\n", id);
 }
 
-void receive_file() {
-  char buffer[bufferSize + 1];
-  //memset (buffer, '\0', bufferSize);
-  int nbytes;
-  bool signal = false;
-  std::string msg, end("\\0");
+void initiate_passiveConnection(int *csocket, struct sockaddr_in* server, char* cport) {  
+  printf("Iniciando conexao passivamente [127.0.0.1|%s].\n", cport);
+  int port = atoi(cport);
+  *csocket = socket(PF_INET, SOCK_STREAM, 0);
+  /*---- Configure settings of the server address struct ----*/  
+  server->sin_family = AF_INET; /* Address family = Internet */  
+  server->sin_port = htons(7891); /* Set port number, using htons function to use proper byte order */  
+  server->sin_addr.s_addr = inet_addr("127.0.0.1"); /* Set IP address to localhost */  
+  memset(server->sin_zero, '\0', sizeof server->sin_zero);  /* Set all bits of the padding field to 0 */
+  bind(*csocket, (struct sockaddr *) server, sizeof(*server)); /*---- Bind the address struct to the socket ----*/
+  printf("Socket aberto, id = %d\n", *csocket);
+}
 
-  while( (nbytes = recv (client_fd, buffer, bufferSize, 0)) ){      
-    //cout << "recebido: " << buffer << "," << nbytes << endl;
+void receive_file(char* filename, int client) {
+  char buffer[bufsize + 1], *packets;
+  FILE *out_file;
+  uint32_t total_bytesSent = 0, total_msgSent = 0;    
+  ssize_t nElem = 0;
+  int nbytes;
+  //  char *response = (char*) malloc(buffer_size);
+  // response[0] = '\0';
+
+  out_file = fopen(filename, "wb+"); // open in binary mode
+  
+  if(out_file == NULL)
+    die(8); //file doesnt exist   
+
+  memset (buffer, '\0', bufsize);
+
+  while( (nbytes = recv (client, buffer, bufsize, 0)) ){      
     if (nbytes < 0){
       die(5); // Error accept
-      //return std::string();
     }
     buffer[nbytes] = '\0';
-    msg += buffer;
-    if(signal and buffer[nbytes-1] == '0') 
-      return msg;
-    if (nbytes > 1 and buffer[nbytes-2] == '\\' and buffer[nbytes-1] == '0')
-      return msg;      
-    if(buffer[nbytes-1] == '\\')
-      signal = true;
+    printf("Recebeu: %s\n", buffer);
+    fwrite(&buffer, 1, nbytes, out_file); // writes file
+
+    /* Montar Pacotes 
+    if (totalBytesRcvd)
+      response = (char*) realloc(response, strlen(response) + strlen(buffer) + 1);
+    strcat(response, buffer);
+    */
   }
-  return std::string();
+  fclose(out_file);
 }
 
 int main(int argc, char **argv){
@@ -46,7 +66,7 @@ int main(int argc, char **argv){
   struct sockaddr_in serverAddr;
   struct sockaddr_storage serverStorage;
   socklen_t addr_size;
-  char* port, in_name, out_name;
+  char* port, *in_name, *out_name;
 
   
   port = argv[1];
@@ -66,7 +86,9 @@ int main(int argc, char **argv){
   addr_size = sizeof serverStorage;
   newSocket = accept(insocket, (struct sockaddr *) &serverStorage, &addr_size);
 
-  receive_file();
+  printf("Connection Accepted\n");
+
+  receive_file(out_name, newSocket);
 
   /*---- Send message to the socket of the incoming connection ----*/
   strcpy(buffer,"Hello World\n");
